cf7fa32ba18b6347cdca39773cd526b5c2751aea
Abhijit <abhijit.bangera@hotmail.com>
2015-10-11 23:54:15 +0530

    updated content

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,8 +24,19 @@ ALLOWED_HOSTS = ['*']
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
+from django.core.mail import send_mail
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend' 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'vbangera64@gmail.com'
+EMAIL_HOST_PASSWORD = 'Maharaja1'
+
+
+
 
 
 # Quick-start development settings - unsuitable for production
--- myportfolio/views.py
+++ myportfolio/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from myportfolio.forms import ContactForm
+
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
+from django.template import Context
+from django.core.mail import send_mail
 
 
 def about(request):
-	return render(request, "index.html", {})
+	form_class = ContactForm
+	context={"name":"Abhijit Bangera",
+			"subtitle":"Programmer",
+			'form': form_class
+			}
+
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+		    subject = form.cleaned_data['subject']
+		    message = form.cleaned_data['message']
+		    sender = form.cleaned_data['sender']
+		    cc_myself = form.cleaned_data['cc_myself']
+		    recipients = ['info@example.com']
+		    if cc_myself:
+		        recipients.append(sender)
+
+		    send_mail(subject, message, sender, recipients)
+
+		    return redirect('/')
+
+
+
+
+
+	return render(request, "index.html", context)
+
+
+
+# add to your views
